f601f2f5c5b6bb8deb2247b95f4b94c22c19229f
Shion Ichikawa <shion1305@gmail.com>
2022-07-04 11:41:16 +0900

    [CHORE] diet code for debug

--- src/main/java/com/shion1305/ynufes/bodytemp2022/config/InstanceData.java
+++ src/main/java/com/shion1305/ynufes/bodytemp2022/config/InstanceData.java
@@ -4,20 +4,13 @@
 
 package com.shion1305.ynufes.bodytemp2022.config;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class InstanceData {
     public String processName;
-    public String gasUrl;
-    public String lineToken;
     public boolean enabled;
 
-    public InstanceData(@JsonProperty("name") String name, @JsonProperty("GASUrl") String getGasUrl, @JsonProperty("LineMessagingToken") String token, @JsonProperty("enabled") boolean enabled) {
+    public InstanceData(String name, boolean enabled) {
         this.processName = name;
-        this.gasUrl = getGasUrl;
-        this.lineToken = token;
         this.enabled = enabled;
     }
 }
--- src/main/java/com/shion1305/ynufes/bodytemp2022/contoller/ProcessorManager.java
+++ src/main/java/com/shion1305/ynufes/bodytemp2022/contoller/ProcessorManager.java
@@ -4,18 +4,14 @@
 
 package com.shion1305.ynufes.bodytemp2022.contoller;
 
-import com.linecorp.bot.model.event.Event;
 import com.shion1305.ynufes.bodytemp2022.config.InstanceData;
-import com.shion1305.ynufes.bodytemp2022.config.JsonConfigManager;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.logging.Logger;
-import java.util.prefs.BackingStoreException;
 
 @WebListener
 public class ProcessorManager implements ServletContextListener {
@@ -28,24 +24,18 @@ public class ProcessorManager implements ServletContextListener {
     }
 
     private static void init() {
-        InstanceData[] data = JsonConfigManager.readJson();
-        if (data == null) return;
-        for (InstanceData d : data) {
-            processors.put(d.processName, new RequestProcessor(d));
-            logger.info(String.format("[%s]Process Registered", d.processName));
+        InstanceData[] data = new InstanceData[5];
+        for (int i = 0; i < 5; i++) {
+            data[i] = new InstanceData("TEST" + String.valueOf(i), true);
+            processors.put("TEST" + String.valueOf(i), new RequestProcessor(data[i]));
         }
     }
 
-
-    /**
-     * 設定の再読み込みを行う。
-     * jsonファイルからボット設定が消去された場合、preferenceは削除されずにインスタンスのみ削除される。
-     * reload処理であるが、reloadableではない(lineTokenが違う)場合は、preferenceが削除され、
-     * インスタンスが再生成される。
-     */
     static void reload() {
-        InstanceData[] data = JsonConfigManager.readJson();
-        if (data == null) return;
+        InstanceData[] data = new InstanceData[5];
+        for (int i = 0; i < 5; i++) {
+            data[i] = new InstanceData("TEST" + String.valueOf(i), false);
+        }
         var newProcessors = new HashMap<String, RequestProcessor>();
         for (InstanceData d : data) {
             if (processors.containsKey(d.processName)) {
@@ -54,8 +44,6 @@ public class ProcessorManager implements ServletContextListener {
                     newProcessors.put(d.processName, process);
                     logger.info(String.format("[%s]Process Reloaded", d.processName));
                     continue;
-                } else {
-                    process.clearPreference();
                 }
             }
             newProcessors.put(d.processName, new RequestProcessor(d));
@@ -65,49 +53,31 @@ public class ProcessorManager implements ServletContextListener {
         processors = newProcessors;
     }
 
-    public static void processEvent(String reqName, Event e) throws BackingStoreException, IOException {
-        RequestProcessor processor = processors.get(reqName);
-        if (processor != null) processor.processEvent(e);
-        else logger.info("Received unknown event: reqName: " + reqName);
-    }
-
     public static void broadcastReminder() {
         for (var processor : processors.values()) {
             processor.broadcastReminder();
         }
     }
 
-    public synchronized static void checkNoSubmission() throws BackingStoreException, IOException {
-        for (var processor : processors.values()) {
-            processor.checkNoSubmission();
-        }
-    }
-
-
     public static class StatusDataManager {
         private static StatusDataGroup data;
 
         public synchronized static StatusDataGroup getStatusData() {
-            if (data == null || System.currentTimeMillis() - data.time > 3000) {
-                data = updateStatusData();
-                logger.info("UPDATED");
-            }
+            data = updateStatusData();
             return data;
         }
 
         private static StatusDataGroup updateStatusData() {
             StatusDataGroup newData = new StatusDataGroup();
             processors.values().stream().parallel().forEach(p ->
-                    newData.addStatus(new StatusData(p.getProcessName(), p.isEnabled(), p.getLineUsage()))
+                    newData.addStatus(new StatusData(p.getProcessName(), p.isEnabled()))
             );
-            newData.time = System.currentTimeMillis();
             return newData;
         }
     }
 
     public static class StatusDataGroup {
         public final ArrayList<StatusData> data = new ArrayList<>();
-        long time = 0L;
 
         public void addStatus(StatusData d) {
             data.add(d);
@@ -117,12 +87,10 @@ public class ProcessorManager implements ServletContextListener {
     public static class StatusData {
         public String processName;
         public boolean enabled;
-        public long usage;
 
-        public StatusData(String processName, boolean enabled, long usage) {
+        public StatusData(String processName, boolean enabled) {
             this.processName = processName;
             this.enabled = enabled;
-            this.usage = usage;
         }
     }
 }
